de9f54e7e69c11d389051d5ac14b63854968c44d
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-07 11:19:53 -0400

    Add data and create folder for routes

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const app = express()
 const cors = require("cors");   
 require('dotenv').config()
 const {PORT} = process.env
+//Importr routes
+const warehouseRoutes = require('./routes/warehouses');
 
 //Middleware
 app.use(cors())
+app.use(express.json()); // to get request body
+app.use(express.static('public')); // to get images as static files
 
 //Routes
-
-
+app.use("/warehouses", warehouseRoutes);
 
 app.listen(PORT, () =>{
     console.log(`Running on port ${PORT}....`)
